85d51b910e9180b52e6248012825c5300ebe093b
Mahnoor Fatima <mahnoorfatima391@gmail.com>
2018-02-16 01:00:30 +0500

    new commit

---
+++ app/src/main/java/com/example/ehsan/safeparking/Login_Fragment.java
@@ -0,0 +1,233 @@
+package com.example.ehsan.safeparking;
+
+/**
+ * Created by ehsan on 03-12-2017.
+ */
+
+import java.util.concurrent.Executor;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import android.app.*;
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.content.res.ColorStateList;
+import android.content.res.XmlResourceParser;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.text.InputType;
+import android.text.TextUtils;
+import android.text.method.HideReturnsTransformationMethod;
+import android.text.method.PasswordTransformationMethod;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.Toast;
+import android.widget.CompoundButton.OnCheckedChangeListener;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class Login_Fragment extends Fragment implements OnClickListener {
+    private static View view;
+
+    private static EditText emailid, password;
+    private static Button loginButton;
+    private static TextView forgotPassword, signUp;
+    private static CheckBox show_hide_password;
+    private static LinearLayout loginLayout;
+    private static Animation shakeAnimation;
+    private static FragmentManager fragmentManager;
+    private FirebaseAuth firebaseAuth;
+    private ProgressDialog progressDialog;
+    Activity mActivity=this.getActivity();
+//    Intent intent=new Intent(getActivity(),Home_Screen.class);
+    public Login_Fragment() {
+
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        progressDialog = new ProgressDialog(getActivity());
+        firebaseAuth = FirebaseAuth.getInstance();
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.login_layout, container, false);
+        initViews();
+        setListeners();
+        return view;
+    }
+
+    // Initiate Views
+    private void initViews() {
+        fragmentManager = getActivity().getSupportFragmentManager();
+
+        emailid = (EditText) view.findViewById(R.id.login_emailid);
+        password = (EditText) view.findViewById(R.id.login_password);
+        loginButton = (Button) view.findViewById(R.id.loginBtn);
+        forgotPassword = (TextView) view.findViewById(R.id.forgot_password);
+        signUp = (TextView) view.findViewById(R.id.createAccount);
+        show_hide_password = (CheckBox) view
+                .findViewById(R.id.show_hide_password);
+        loginLayout = (LinearLayout) view.findViewById(R.id.login_layout);
+
+        // Load ShakeAnimation
+        shakeAnimation = AnimationUtils.loadAnimation(getActivity(),
+                R.anim.shake);
+
+        // Setting text selector over textviews
+        @SuppressLint("ResourceType") XmlResourceParser xrp = getResources().getXml(R.drawable.text_selector);
+        try {
+            ColorStateList csl = ColorStateList.createFromXml(getResources(),
+                    xrp);
+
+            forgotPassword.setTextColor(csl);
+            show_hide_password.setTextColor(csl);
+            signUp.setTextColor(csl);
+        } catch (Exception e) {
+        }
+    }
+    // Set Listeners
+    private void setListeners() {
+        loginButton.setOnClickListener(this);
+        forgotPassword.setOnClickListener(this);
+        signUp.setOnClickListener(this);
+
+        // Set check listener over checkbox for showing and hiding password
+        show_hide_password
+                .setOnCheckedChangeListener(new OnCheckedChangeListener() {
+
+                    @Override
+                    public void onCheckedChanged(CompoundButton button,
+                                                 boolean isChecked) {
+
+                        // If it is checkec then show password else hide
+                        // password
+                        if (isChecked) {
+
+                            show_hide_password.setText(R.string.hide_pwd);// change
+                            // checkbox
+                            // text
+
+                            password.setInputType(InputType.TYPE_CLASS_TEXT);
+                            password.setTransformationMethod(HideReturnsTransformationMethod
+                                    .getInstance());// show password
+                        } else {
+                            show_hide_password.setText(R.string.show_pwd);// change
+                            // checkbox
+                            // text
+
+                            password.setInputType(InputType.TYPE_CLASS_TEXT
+                                    | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+                            password.setTransformationMethod(PasswordTransformationMethod
+                                    .getInstance());// hide password
+
+                        }
+
+                    }
+                });
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.loginBtn:
+                checkValidation();
+                break;
+
+            case R.id.forgot_password:
+
+                // Replace forgot password fragment with animation
+                fragmentManager
+                        .beginTransaction()
+                        .setCustomAnimations(R.anim.right_enter, R.anim.left_out)
+                        .replace(R.id.frameContainer,
+                                new ForgotPassword_Fragment(),
+                                Utils.ForgotPassword_Fragment).commit();
+                break;
+            case R.id.createAccount:
+
+                // Replace signup frgament with animation
+                fragmentManager
+                        .beginTransaction()
+                        .setCustomAnimations(R.anim.right_enter, R.anim.left_out)
+                        .replace(R.id.frameContainer, new SignUp_Fragment(),
+                                Utils.SignUp_Fragment).commit();
+                break;
+        }
+
+    }
+
+    // Check Validation before login
+    private void checkValidation() {
+        // Get email id and password
+        String getEmailId = emailid.getText().toString();
+        String getPassword = password.getText().toString();
+
+        // Check patter for email id
+        Pattern p = Pattern.compile(Utils.regEx);
+
+        Matcher m = p.matcher(getEmailId);
+
+        // Check for both field is empty or not
+        if (getEmailId.equals("") || getEmailId.length() == 0
+                || getPassword.equals("") || getPassword.length() == 0) {
+            loginLayout.startAnimation(shakeAnimation);
+            new CustomToast().Show_Toast(getActivity(), view,
+                    "Enter both credentials.");
+
+        }
+        // Check if email id is valid or not
+        else if (!m.find()) {
+            loginLayout.startAnimation(shakeAnimation);
+            new CustomToast().Show_Toast(getActivity(), view,
+                    "Your Email Id is Invalid.");
+        }
+            // Else do login and do your stuff
+        else {
+            Login.isLoggedIn=true;
+            userLogin();
+        }
+    }
+
+    private void userLogin(){
+        String email = emailid.getText().toString().trim();
+        String pswd = password.getText().toString().trim();
+        progressDialog.setMessage("Please wait...");
+           progressDialog.show();
+        firebaseAuth.signInWithEmailAndPassword(email, pswd).addOnCompleteListener(getActivity(), new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                 progressDialog.dismiss();
+                if (task.isSuccessful()){
+                    Toast.makeText(getActivity(),"User registered", Toast.LENGTH_SHORT).show();
+                    getActivity().finish();
+                    getActivity().startActivity(new Intent(getActivity(), Home_Screen.class));
+                }
+                else{
+                    Toast.makeText(getActivity(),"Cannot connect to internet", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+    }
+}
